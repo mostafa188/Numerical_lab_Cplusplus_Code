2db05b6b4f2e908abc66e131cbd9227318fd12d8
Mostafa kamal <110708254+mostafa188@users.noreply.github.com>
2023-07-10 09:08:27 +0600

    Multiple roots

--- 7_muller.cpp
+++ 7_muller.cpp
@@ -1,61 +1,56 @@
-#include<bits/stdc++.h>
-using namespace std;
-int MAX_ITERATIONS = 10000;
-
+#include <stdio.h>
+#include <math.h>
+const int MAX = 10000;
 float f(float x)
 {
-	
-	return 1*pow(x, 3) + 2*x*x + 10*x - 20;
+    return 1 * pow(x, 3) + 2 * x * x + 10 * x - 20;
 }
-
-void Muller(float a, float b, float c)
+int muller(float a, float b, float c)
 {
-	int i;
-	float res;
+    int i;
+    float result;
+    for (i = 0;; ++i)
+    {
+        float f1 = f(a);
+        float f2 = f(b);
+        float f3 = f(c);
+        float d1 = f1 - f3;
+        float d2 = f2 - f3;
+        float h1 = a - c;
+        float h2 = b - c;
+        float cc = f3;
+        float bb = (((d2 * pow(h1, 2)) - (d1 * pow(h2, 2))) / ((h1 * h2) * (h1 - h2)));
+        float aa = (((d1 * h2) - (d2 * h1)) / ((h1 * h2) * (h1 - h2)));
+        float x = ((-2 * cc) / (bb + abs(sqrt(bb * bb - 4 * cc * aa))));
+        float y = ((-2 * cc) / (bb - abs(sqrt(bb * bb - 4 * cc * aa))));
 
-	for (i = 0;;++i)
-	{
-		float f1 = f(a);
-		float f2 = f(b);
-		float f3 = f(c);
-		float d1 = f1 - f3;
-		float d2 = f2 - f3;
-		float h1 = a - c;
-		float h2 = b - c;
-		float a0 = f3;
-		float a1 = (((d2*pow(h1, 2)) - (d1*pow(h2, 2)))
-					/ ((h1*h2) * (h1-h2)));
-		float a2 = (((d1*h2) - (d2*h1))/((h1*h2) * (h1-h2)));
-		float x = ((-2*a0) / (a1 + abs(sqrt(a1*a1-4*a0*a2))));
-		float y = ((-2*a0) / (a1-abs(sqrt(a1*a1-4*a0*a2))));
+        if (x >= y)
+            result = x + c;
+        else
+            result = y + c;
 
-		if (x >= y)
-			res = x + c;
-		else
-			res = y + c;
-
-		float m = res*100;
-		float n = c*100;
-		m = floor(m);
-		n = floor(n);
-		if (m == n)
-			break;
-		a = b;
-		b = c;
-		c = res;
-		if (i > MAX_ITERATIONS)
-		{
-			cout << "Root cannot be found using"
-				<< " Muller's method";
-			break;
-		}
-	}
-	if (i <= MAX_ITERATIONS)
-		cout << "The value of the root is " << res;
+        float m = result * 10000;
+        float n = c * 10000;
+        m = floor(m);
+        n = floor(n);
+        if (m == n)
+            break;
+        a = b;
+        b = c;
+        c = result;
+        if (i > MAX)
+        {
+            printf("Root can't be found using Muller method\n");
+            break;
+        }
+    }
+    if (i <= MAX)
+        printf("The root is %f", result);
+    return 0;
 }
 int main()
 {
-	float a = 0, b = 1, c = 2;
-	Muller(a, b, c);
-	return 0;
+    float a = 1, b = 3, c = 2;
+    muller(a, b, c);
+    return 0;
 }
